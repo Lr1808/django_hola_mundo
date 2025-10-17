6305121b0ddd4fa66fd6f26424286be486180408
Lr1808 <rodulfoluis1808@gmail.com>
2025-10-16 18:34:47 -0700

    about page listo

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+# Create your views here.
 class HomeView(TemplateView):
     template_name = "home.html"
 
-
-
-
-# Create your views here.
+class AboutView(TemplateView):
+    template_name = "about.html"
